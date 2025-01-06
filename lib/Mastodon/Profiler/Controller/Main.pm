0b2f165294208a144309becc3ceee6fb40817a77
Gene Boggs <gene@ology.net>
2025-01-05 18:10:34 -0800

    WIP

--- lib/Mastodon/Profiler/Controller/Main.pm
+++ lib/Mastodon/Profiler/Controller/Main.pm
@@ -7,17 +7,16 @@ use Mojo::UserAgent ();
 use Try::Tiny;
 
 sub index ($self) {
-  my $server = $self->param('server') || '';
-  my $user   = $self->param('user') || '';
+  my $profile = $self->param('profile') || '';
   $self->render(
-    server => $server,
-    user   => $user,
+    profile  => $profile,
+    response => undef,
   );
 }
 
 sub profiler ($self) {
-  my $server = $self->param('server') || '';
-  my $user = $self->param('user') || '';
+  my $profile = $self->param('profile') || '';
+  my (undef, $user, $server) = split '@', $profile;
   my $uri = Mojo::URL->new("https://$server")
     ->path('api/v1/accounts/lookup')
     ->query(acct => $user);
@@ -26,9 +25,7 @@ sub profiler ($self) {
   my $response = _handle_response($tx);
   $self->render(
     template => 'main/index',
-    server   => $server,
-    user     => $user,
-    uri      => $uri,
+    profile  => $profile,
     response => $response,
   );
 }
