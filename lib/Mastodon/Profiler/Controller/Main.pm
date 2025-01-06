624f49cedd8e30a570caff29404276271340c502
Gene Boggs <gene@ology.net>
2025-01-05 16:08:16 -0800

    WIP

--- lib/Mastodon/Profiler/Controller/Main.pm
+++ lib/Mastodon/Profiler/Controller/Main.pm
@@ -1,7 +1,10 @@
 package Mastodon::Profiler::Controller::Main;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
-use URI ();
+use Mojo::JSON qw(decode_json);
+use Mojo::URL ();
+use Mojo::UserAgent ();
+use Try::Tiny;
 
 sub index ($self) {
   my $server = $self->param('server') || '';
@@ -15,15 +18,37 @@ sub index ($self) {
 sub profiler ($self) {
   my $server = $self->param('server') || '';
   my $user = $self->param('user') || '';
-  my $uri = URI->new("https://$server");
-  $uri->path('api/v1/accounts/lookup');
-  $uri->query_form(acct => $user);
+  my $uri = Mojo::URL->new("https://$server")
+    ->path('api/v1/accounts/lookup')
+    ->query(acct => $user);
+  my $ua = Mojo::UserAgent->new;
+  my $tx = $ua->get($uri);
+  my $data = _handle_response($tx);
   $self->render(
     template => 'main/index',
     server   => $server,
     user     => $user,
-    uri      => $uri->as_string,
+    uri      => $uri,
   );
+}
+
+sub _handle_response {
+    my ($tx) = @_;
+    my $data;
+    my $res = $tx->result;
+    if ( $res->is_success ) {
+      my $body = $res->body;
+      try {
+        $data = decode_json($body);
+      }
+      catch {
+        die $body, "\n";
+      };
+    }
+    else {
+      die "Connection error: ", $res->message, "\n";
+    }
+    return $data;
 }
 
 1;
